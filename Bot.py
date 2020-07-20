6a4cb15c574e1993b54777bc267833b24f7a3350
Heartbreaker <lazyheartbraker@gmail.com>
2020-07-20 07:39:26 +0200

    Made both the bot and window start at same time still no conection between them

--- Bot.py
+++ Bot.py
@@ -2,11 +2,14 @@ import discord
 from discord.ext import commands
 from hidden import my_token as TOKEN
 import asyncio
+import threading
 
-#from window import startwindow
+
+client = commands.Bot(command_prefix="!")
 
 
-client = commands.Bot(command_prefix = "!")
+async def start():
+    await client.start(TOKEN)
 
 
 @client.event
@@ -14,9 +17,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #window.add_to_msgs(f"{message.author}: {message.content}")
+    # window.add_to_msgs(f"{message.author}: {message.content}")
     print(f"{message.author} in {message.channel.name}: {message.content}")
     await client.process_commands(message)
+
 
 @client.event
 async def on_ready():
@@ -24,11 +28,3 @@ async def on_ready():
     print("{0.user}".format(client))
     print(client.user.id)
     print("------")
-
-
-#loop = asyncio.get_event_loop()
-#loop.create_task(client.start(TOKEN))
-#loop.create_task(startwindow())
-#loop.run_forever()
-#client.run()
-asyncio.get_event_loop.run_forever(client.start(TOKEN))
--- window.py
+++ window.py
@@ -2,40 +2,40 @@ import sys
 import asyncio
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from Ui_MainWindow import Ui_MainWindow
-import time
 import Bot
+
 
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        #self.send.clicked.connect(self.on_button)
+        # self.send.clicked.connect(self.on_button)
+        self.threadpool = QtCore.QThreadPool()
 
-        self.myThread = DiscordThread()
-        self.myThread.start()
+        worker = Worker()
+        self.threadpool.start(worker)
 
 
 #    def on_button(self):
 
 
-    #def add_to_msgs(self, text):
-     #   self.msg_window.append(text)
+# def add_to_msgs(self, text):
+#   self.msg_window.append(text)
 
 
-class DiscordThread(QtCore.QThread):
+class Worker(QtCore.QRunnable):
+    """
+    Worker thread
+    """
+
     def __init__(self):
-        QtCore.QThread.__init__(self)
+        super(Worker, self).__init__()
+        self.loop = asyncio.get_event_loop()
 
-    def __del__(self):
-        self.wait()
-
- 
     def run(self):
-        #loop = asyncio.get_event_loop()
-        #loop.create_task(Bot.client.start(Bot.TOKEN))
-        #loop.run_forever()
-        asyncio.run(Bot.client.start(Bot.TOKEN))
+        self.loop.create_task(Bot.start())
+        self.loop.run_forever()
 
 
 if __name__ == "__main__":
